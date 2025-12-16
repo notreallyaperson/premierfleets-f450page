afea70325643b2cc13f0ea37aab6a0b68cb4e181
Avinash <avinash@mindset.swiss>
2025-12-17 01:09:12 +0800

    fix deployment

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// server.js
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
@@ -10,10 +11,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "dist")));
 
-app.get("*", (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
